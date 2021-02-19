832131481e3c6df7ba1ab6e968dea6e4b5283808
dviedma <dviedm@gmail.com>
2021-02-18 23:35:04 -0500

    background noise in comment

--- components/play/Play.js
+++ components/play/Play.js
@@ -10,6 +10,9 @@ const Play = (props) => {
       <div className="row pr-3">
         <div className="col-md-8 col-sm-12">
           <div id="play-video-container" style={{height: 0,width: "100%",paddingBottom: "57%",backgroundColor: "rgba(102, 102, 102, 1)",borderRadius: "0.75em"}}>
+            {/*          <div id="play-video-container" 
+            style={{height: 0,width: "100%",paddingBottom: "57%",background: "url(https://media3.giphy.com/media/etUdXnsEMrBng0vOvY/200.gif) no-repeat",
+            borderRadius: "0.75em",backgroundSize:"110%",backgroundPosition:"-10px" }}> */}
             <Player />
           </div>
         </div>
