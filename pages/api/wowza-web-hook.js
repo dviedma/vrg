18048fc26006489004c3083461f4367ba70f56f9
dviedma <dviedm@gmail.com>
2021-02-27 11:01:46 -0500

    adding console log

--- pages/api/wowza-web-hook.js
+++ pages/api/wowza-web-hook.js
@@ -1,6 +1,5 @@
 export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
   if (req.method === 'POST') {
-    // Process a POST request
+    console.log(">>> POST:", req);
   }
 }
