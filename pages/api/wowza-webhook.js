c2212dda5d398d81ee54f634a8322fd57d5baad0
dviedma <dviedm@gmail.com>
2021-03-03 20:50:49 -0500

    mnodified hook

--- pages/api/wowza-webhook.js
+++ pages/api/wowza-webhook.js
@@ -3,17 +3,15 @@ import fire from '../../config/fire-config';
 export default function handler(req, res) {
   console.log(">>> Hi Hook req:", req.body);
   
-  /*
-  if (req.method === 'POST' && (req.body.action == "video.started" || req.body.action == "video.stopped")) {
+  if (req.method === 'POST' && (req.body.event == "video.started" || req.body.event == "video.stopped")) {
     console.log(">>> Adding object to DB");
     fire.firestore()
       .collection('wowzaevents')
       .add({
         channelId: req.body.object_id,
-        action: req.body.action
+        event: req.body.event
       });
   }
-  */
   
   res.status(200).json(req)
 }
