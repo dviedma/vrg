37cb763f4074a3f624745f07a1db9e7be7b8850e
dviedma <dviedm@gmail.com>
2021-03-04 09:21:03 -0500

    webhook deploy

--- pages/api/wowza-webhook.js
+++ pages/api/wowza-webhook.js
@@ -3,15 +3,24 @@ import fire from '../../config/fire-config';
 export default function handler(req, res) {
   console.log(">>> Hi Hook req:", req.body);
   
-  if (req.method === 'POST' && (req.body.event == "video.started" || req.body.event == "video.stopped")) {
+  //if (req.method === 'POST' && (req.body.event == "video.started" || req.body.event == "video.stopped")) {
     console.log(">>> Adding object to DB");
-    fire.firestore()
-      .collection('wowzaevents')
-      .add({
-        channelId: req.body.object_id,
-        event: req.body.event
-      });
-  }
+
+    fire.auth()
+      .signInWithEmailAndPassword("wowza@wowza.com", "Wowza123!")
+      .then((result) => {
+        fire.firestore()
+        .collection('wowzaevents')
+        .add({
+          channelId: req.body.object_id,
+          event: req.body.event
+        });
+      })
+      .catch((err) => {
+        console.log(err.code, err.message)
+      })    
+
+  //}
   
   res.status(200).json(req)
 }
