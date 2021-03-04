167bfcd76086d6f39c55858603bdba318c88e201
dviedma <dviedm@gmail.com>
2021-03-04 13:23:48 -0500

    adding conditions

--- pages/api/wowza-webhook-test.js
+++ pages/api/wowza-webhook-test.js
@@ -1,22 +1,23 @@
 import fire from '../../config/fire-config';
 
 export default function handler(req, res) {
-  console.log("req.method", req.method, req.body.event);
-  if (req.method == "POST" && (req.body.event == "video.started" || req.body.event == "video.stopped")) {
+  //console.log("fire", fire);
+
+  //if (req.method == "POST") {
     console.log("adding to DB with req.body.event_time", req.body.event_time);
     fire.firestore()
       .collection('wowzaevents')
       .add({
-        channelId: req.body.object_id,
-        event: req.body.event,
-        timestamp: fire.firestore.FieldValue.serverTimestamp()
+        channelId: "test",
+        event: "test",
+        timestamp: "test"
       })
       .then((result) => {
-        console.log("Success adding", result);
+        console.log("Success adding");
       })
       .catch((err) => {
         console.log(err.code, err.message)
       })    
-  }
+  //}
   res.status(200).json(req)
 }
